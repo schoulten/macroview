da60bbda4a77d38d6843bad2a6c38f61d8427838
Fernando da Silva <fernando.schoulten@hotmail.com>
2021-04-01 16:54:23 -0300

    small improvements in setup chunk of index.rmd

--- R/testing.R
+++ R/testing.R
@@ -1,22 +1,14 @@
-
-
-retry(etl_econ_activity())
-retry(etl_fiscal())
-retry(etl_inflation())
-retry(etl_international())
-retry(etl_labor())
-retry(etl_monetary())
-
-
-retry(rmarkdown::render("index.Rmd"))
-
-
-
-devtools::document()
-devtools::load_all()
-devtools::check()
-
-
-
-library(tidyverse)
-mutate(mtcars, across(where(is.numeric), ~as.character))
+# devtools::load_all()
+#
+# retry(etl_econ_activity())
+# retry(etl_fiscal())
+# retry(etl_inflation())
+# retry(etl_international())
+# retry(etl_labor())
+# retry(etl_monetary())
+#
+#
+# retry(rmarkdown::render("index.Rmd"))
+#
+#
+# devtools::check()
